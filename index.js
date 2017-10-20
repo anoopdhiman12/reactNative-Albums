bad4eea90b1d6a177cc909aec9c2b19fc308bb59
projectmanager1221 <projectmanager1221@gmail.com>
2017-10-21 02:37:49 +0530

    List Component Boilerplate + return single jsx tab in APP root level

--- index.js
+++ index.js
@@ -7,14 +7,18 @@
 
 //import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
  
 //create component
 
 const App = () => (
+    <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+    </View>
 );
 
 //render
---
+++ src/components/AlbumList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const  AlbumList = () => {
+    return (
+        <View>
+            <Text>AlbumList</Text>
+        </View>
+    );
+};
+export default AlbumList;
