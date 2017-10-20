1dfa0e177cc95451fda5e928116854f8e7ef75ca
projectmanager1221 <projectmanager1221@gmail.com>
2017-10-21 01:37:07 +0530

    styling with View

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,24 @@
 //import libraries to make components
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 //make component
 
 const Header = () => {
-    const { textStyle } = styles;
 
-  return <Text style={textStyle}>Albums</Text>;
+    const { textStyle, viewStyle } = styles;
+
+  return (
+  <View style={viewStyle}>
+  <Text style={textStyle}>Albums</Text>
+  </View>
+  );
     };
 
 //styling
 const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
     textStyle: {
         fontSize: 20
     }
