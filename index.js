d3e84043e41642cde733d04a95be4da917d6ef0b
projectmanager1221 <projectmanager1221@gmail.com>
2017-10-21 00:59:55 +0530

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+
+//import { AppRegistry } from 'react-native';
+//import App from './App';
+
+//AppRegistry.registerComponent('albums', () => App);
+
+
+//import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+ 
+//create component
+
+const App = () => (
+    <Header/>
+);
+
+//render
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+//import libraries to make components
+import React from 'react';
+import { Text } from 'react-native';
+//make component
+
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+
+//make component available for other parts
+export default Header;
