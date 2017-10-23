d11fa501ab9eea845c4b5ce8f1807a19d05e24b4
projectmanager1221 <projectmanager1221@gmail.com>
2017-10-23 15:34:44 +0530

    map function for array

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,16 +4,23 @@ import axios from 'axios';
 
 
 class AlbumList extends Component {
+state = { albums: [] };
+
 componentWillMount() {
     //console.log('component will mount done');
-    axios.get('http://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response) 
-    );
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data })); 
 }
+
+renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+}
+
     render() {
+        ///console.log(this.state);
     return (
         <View>
-            <Text>AlbumList</Text>
+            {this.renderAlbums()}
         </View>
     );
 }
