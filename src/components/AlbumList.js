14c1544ca99af92b5c9124a2fc018ee83aa990b6
projectmanager1221 <projectmanager1221@gmail.com>
2017-10-23 14:26:40 +0530

    axios http request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,14 @@
 import React, {Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+
 
 class AlbumList extends Component {
 componentWillMount() {
-    console.log('component will mount done');
+    //console.log('component will mount done');
+    axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response) 
+    );
 }
     render() {
     return (
