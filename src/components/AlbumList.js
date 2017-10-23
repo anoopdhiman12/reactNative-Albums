22cc90303ff1e819615b5ce94abdc6b83a709c5c
projectmanager1221 <projectmanager1221@gmail.com>
2017-10-23 15:36:50 +0530

    key

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -13,7 +13,7 @@ componentWillMount() {
 }
 
 renderAlbums() {
-    return this.state.albums.map(album => <Text>{album.title}</Text>);
+    return this.state.albums.map(album => <Text key={album.title} >{album.title}</Text>);
 }
 
     render() {
