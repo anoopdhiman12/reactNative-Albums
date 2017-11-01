51bcd854ad7f6f155603c5e8e47eba541bb19360
projectmanager1221 <projectmanager1221@gmail.com>
2017-11-01 19:10:50 +0530

    working with Image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,21 +4,27 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 
-const AlbumDetail = ({album }) => {
+const AlbumDetail = ({ album }) => {
 
-    const { title, artist, thumbnail_image } = album;
-
+    const { title, artist, thumbnail_image, image } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
+    
     return (
       <Card>
           <CardSection>
-              <View>
-                  <Image source={{ uri: thumbnail_image }} />
+              <View style={thumbnailContainerStyle} >
+                  <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
               </View>
-              <View style={styles.headerContentStyle}>
-          <Text> {title} </Text>
+              <View style={headerContentStyle}>
+          <Text style={headerTextStyle}> {title} </Text>
           <Text> {artist} </Text>
           </View>
           </CardSection>
+          
+          <CardSection>
+              <Image style={imageStyle} source={{ uri: image }} />
+          </CardSection>
+    
       </Card>  
     );
 };
@@ -27,8 +33,25 @@ const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
-
 };
 
 export default AlbumDetail;
