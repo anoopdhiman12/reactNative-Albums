00512871a4516c0145b21ebed2d087dd950566ee
projectmanager1221 <projectmanager1221@gmail.com>
2017-11-09 04:40:19 +0530

    styling

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
 
 //AppRegistry.registerComponent('albums', () => App);
 
-
 //import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
- 
+
 //create component
 
 const App = () => (
