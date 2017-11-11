4bae335d28c52f99f3fcb71fc3b0982e991d1e56
projectmanager1221 <projectmanager1221@gmail.com>
2017-11-11 06:09:55 +0530

    reusable Button with Prop

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -27,7 +27,9 @@ const AlbumDetail = ({ album }) => {
           </CardSection>
           
           <CardSection>
-              <Button onPress={() => Linking.openURL(url)} />
+              <Button onPress={() => Linking.openURL(url)}>
+              Buy Now
+              </Button>
         </CardSection>
     
       </Card>  
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
     const { buttonStyle, textStyle } = styles;
 
 
     return (
         <TouchableOpacity onPress={onPress} style={buttonStyle}>
     <Text style={textStyle}>
-    Click Me !!!
+    {children}
     </Text>
     </TouchableOpacity >
     );
